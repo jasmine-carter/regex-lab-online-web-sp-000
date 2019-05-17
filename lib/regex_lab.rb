d6b9833d0fab066e30a10d94ee4a252a09973833
Jasmine Carter <jasmine.carter87@gmail.com>
2019-05-17 00:27:30 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,17 +1,21 @@
 def starts_with_a_vowel?(word)
-
+if word.match(/\A+[AaEeIiOoUu]/)
+true
+else
+  false
+end
 end
 
-def words_starting_with_un_and_ending_with_ing(text)
-
+def words_starting_with_un_and_ending_with_ing(words_string)
+words_string.scan(/\w+ing/)
 end
 
-def words_five_letters_long(text)
-
+def words_five_letters_long(words_string)
+words_string.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+text.match(/\A+[A-Z])
 end
 
 def valid_phone_number?(phone)
